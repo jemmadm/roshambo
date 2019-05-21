897acdb45166dc13b1288c32bb017f27242256eb
Jemma <jemmamillman@gmail.com>
2019-05-21 12:05:11 +0100

    added best of 3 and final winner

--- roshambo.js
+++ roshambo.js
@@ -1,6 +1,9 @@
 var activePlayer = 0;
 var player1Move = null;
 var player2Move = null;
+var count = 0;
+var player1Winner = null;
+var player2Winner = null;
 
 function changePlayer() {
     activePlayer = activePlayer % 2 + 1;
@@ -22,12 +25,14 @@ function checkForWinner() {
             player1Move === 'paper' && player2Move === 'spock'
         ) {
             document.getElementById("winner").innerHTML = "Player 1 wins";
+            player1Winner += 1;
             resetGame();
         } else if (player1Move === player2Move) {
             document.getElementById("winner").innerHTML = "It's a draw";
             resetGame()
         } else {
             document.getElementById("winner").innerHTML = "Player 2 wins";
+            player2Winner += 1;
             resetGame()
         }
     }
@@ -39,11 +44,30 @@ function resetGame() {
 }
 
 function play(moveName) {
+
     if (activePlayer === 1) {
         player1Move = moveName;
     } else {
         player2Move = moveName;
     }
+    count++;
     checkForWinner();
     changePlayer();
+
+    if (count === 6) {
+        checkFinalWinner();
+    }
+}
+
+function checkFinalWinner() {
+
+    if (player1Winner > player2Winner) {
+        document.getElementById("finalWinner").innerHTML = "The final winner is Player 1";
+    } else if (player1Winner === player2Winner) {
+        document.getElementById("finalWinner").innerHTML = "Final winner: It's a draw";
+    } else {
+        document.getElementById("finalWinner").innerHTML = "The final winner is Player 2";
+    }
+    count = 0;
+    resetGame();
 }
