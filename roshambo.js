3a06babe4ad740c44ef9f03a7c8c4439e605323c
Jemma <jemmamillman@gmail.com>
2019-05-21 10:33:54 +0100

    original exercise completed and Lizard Spock added

--- roshambo.js
+++ roshambo.js
@@ -2,26 +2,44 @@ var activePlayer = 0;
 var player1Move = null;
 var player2Move = null;
 
-function changePlayer(){
+function changePlayer() {
     activePlayer = activePlayer % 2 + 1;
     document.getElementById("activePlayer").innerHTML =
         "It's player " + activePlayer + "'s turn!";
 }
 
-function checkForWinner(){
-    if( player1Move && player2Move ){
-        document.getElementById("winner").innerHTML = "Who wins!";
-        resetGame();
+function checkForWinner() {
+    if (player1Move && player2Move) {
+        if (player1Move === 'paper' && player2Move === 'rock' ||
+            player1Move === 'rock' && player2Move === 'scissors' ||
+            player1Move === 'scissors' && player2Move === 'paper' ||
+            player1Move === 'lizard' && player2Move === 'spock' ||
+            player1Move === 'lizard' && player2Move === 'paper' ||
+            player1Move === 'spock' && player2Move === 'scissors' ||
+            player1Move === 'spock' && player2Move === 'rock' ||
+            player1Move === 'scissors' && player2Move === 'lizard' ||
+            player1Move === 'rock' && player2Move === 'lizard' ||
+            player1Move === 'paper' && player2Move === 'spock'
+        ) {
+            document.getElementById("winner").innerHTML = "Player 1 wins";
+            resetGame();
+        } else if (player1Move === player2Move) {
+            document.getElementById("winner").innerHTML = "It's a draw";
+            resetGame()
+        } else {
+            document.getElementById("winner").innerHTML = "Player 2 wins";
+            resetGame()
+        }
     }
 }
 
-function resetGame(){
+function resetGame() {
     player1Move = null;
     player2Move = null;
 }
 
 function play(moveName) {
-    if( activePlayer === 1 ) {
+    if (activePlayer === 1) {
         player1Move = moveName;
     } else {
         player2Move = moveName;
